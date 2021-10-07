89b62876fc3bdb0198560571717ea4fd84eab668
Flavio <68715389+Flavio-LP@users.noreply.github.com>
2021-10-07 14:40:05 -0300

    atualização podcast React

--- src/Components/Player/index.tsx
+++ src/Components/Player/index.tsx
@@ -4,7 +4,18 @@ export function Player(){
 
     return(
         <div className={styles.playerContainer}>
-            
+            <header>
+                <img src="/playing.svg" alt="Tocando agora"></img>
+                <strong>Trocando agora</strong>
+            </header>
+            <div>
+                <strong>Selecione um podcast para ouvir</strong>
+            </div>
+
+            <footer>
+                
+            </footer>
+
         </div>
         
     );
