b17c4bfb8b1d8577217ea5cec44ca029dc778185
Flavio-LP <68715389+Flavio-LP@users.noreply.github.com>
2021-10-08 16:59:04 -0300

    implantação do axios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,22 @@
-import { useEffect } from "react";
+//import { useEffect } from "react";
+import { GetServerSideProps } from "next";
+import { api } from "../services/api";
+import {format,parseISO} from 'date-fns';
 
-export default function Home(props){
+type Episode = {
+  episodes: Array<{
+    id: string;
+    title: string;
+    members: string;
+    published_at: string;
+  }>
+}
+
+type HomeProps={
+  episodes: Episode[];
+}
+
+export default function Home(props: HomeProps){
     
   /*useEffect(() =>{
     fetch('http://localhost:3333/episodes')
@@ -18,9 +34,25 @@ return(
 
 }
 
-export async function getServerSideProps(){
-  const response= await fetch('http://localhost:3333/episodes');
-  const data= await response.json()
+export  const getServerSideProps: GetServerSideProps = async () => {
+  const response= await api.get('episodes',{
+    params:{
+      _limit:12,
+      _sort:'published_at',
+      _order: 'desc'
+    }
+  });
+  const data= await response.data
+
+  const episodes= data.map ( episode=>{
+    return{
+      id:episode.id,
+      title:episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt:episode.published_at
+    }
+  })
 
   return{
     props:{
